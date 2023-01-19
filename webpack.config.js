80725ecd2b090c62d0821c0c6b5095bc300e0edf
Yair-N <ynotkovich@gmail.com>
2023-01-19 18:47:42 +0200

    updated devDependencies and webpack.config to allow interpulation of %PUBLIC_URL%

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 //webpack.config.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const InterpolateHtmlPlugin = require('interpolate-html-plugin');
+
+
 module.exports = {
     entry: './src/index.js',
+    resolve: {
+        alias: {
+            "styled-components": path.resolve(__dirname, "node_modules", "styled-components"),
+        }
+    },
     output: {
         path: path.join(__dirname, '/dist'),
         filename: 'bundle.js'
     },
     devServer: {
-        port: 8080
+        port: 3000
     },
     module: {
         rules: [
@@ -20,12 +28,32 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-url-loader',
+                        options: {
+                            limit: 10000,
+                        },
+                    },
+                ],
             }
         ]
     },
     plugins: [
+        new InterpolateHtmlPlugin({ PUBLIC_URL: path.join(__dirname, '/public') }),
         new HtmlWebpackPlugin({
+            inject: true,
             template: path.join(__dirname, '/public/index.html')
-        })
+        }),
+
+
     ]
 }
